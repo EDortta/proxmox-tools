8794db46b9a9199d50e434a088b46d46a711e417
Esteban Daniel Dortta <dortta@yahoo.com>
2025-06-27 13:47:58 -0300

    Better view-ips.sh script

--- view-ips.sh
+++ view-ips.sh
@@ -10,14 +10,13 @@ echo "----------------------------------------------------------"
 echo -e "VMID\tHostname\t\tExt.IP\t\tInt.IP"
 echo "----------------------------------------------------------"
 
-pct list | grep -w running | awk '$1 >= $VMID {print $1}' | while read -r vmid; do
+while read -r vmid; do
     hostname=$(pct exec "$vmid" -- hostname)
 
     ip0=$(pct exec "$vmid" -- ip -4 addr show dev eth0 2>/dev/null | grep 'inet ' | awk '{print $2}' | cut -d'/' -f1)
-
     ip1=$(pct exec "$vmid" -- ip -4 addr show dev eth1 2>/dev/null | grep 'inet ' | awk '{print $2}' | cut -d'/' -f1)
 
     printf "%-5s\t%-20s\t%s %s\n" "$vmid" "$hostname" "${ip0:-unattached}" "${ip1:-unattached}"
-done
-echo "----------------------------------------------------------"
+done < <(pct list | awk -v vmid="$VMID" '$1 >= vmid && $2 == "running" {print $1}')
 
+echo "----------------------------------------------------------"
